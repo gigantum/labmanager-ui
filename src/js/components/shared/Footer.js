7496787a8c2224f74ce4758b99d43869958e978e
Calum Butler <calum@flashx.io>
2017-08-24 09:58:46 -0400

    fixed variable names in footer and added comments

--- src/js/components/shared/Footer.js
+++ src/js/components/shared/Footer.js
@@ -3,26 +3,26 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
 
   componentDidMount() {
+
+    //adds a class to the footer element to expand when the user scolls to the bottom of the screen
     window.addEventListener('scroll', function(e){
       let root = document.getElementById('root')
       let distanceY = window.innerHeight + document.body.scrollTop + 20,
-          shrinkOn = root.offsetHeight,
+          expandOn = root.offsetHeight,
           footer = document.getElementById("footer");
 
-      if (distanceY > shrinkOn) {
+      if (distanceY > expandOn) {
           footer.classList.add("Footer__expand");
       } else {
           if (footer.classList.contains("Footer__expand")) {
-              footer.classList.remove("Footer__expand");
-          }
-      }
-  });
+                footer.classList.remove("Footer__expand");
+            }
+        }
+    });
   }
   render() {
     return (
       <div id="footer" className={'Footer flex flex--row justify--space-between'}>
-
-
 
       </div>
     )
