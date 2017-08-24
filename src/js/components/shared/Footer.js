f6c68a3b1f43490cb042ca2a4aa9f262c1977052
Calum Butler <calum@flashx.io>
2017-08-24 09:44:48 -0400

    cleaned up footer code and removed experimental drag and drop from user note

--- src/js/components/shared/Footer.js
+++ src/js/components/shared/Footer.js
@@ -4,13 +4,11 @@ export default class Footer extends Component {
 
   componentDidMount() {
     window.addEventListener('scroll', function(e){
-      console.log(e)
-      var root = document.getElementById('root')
-      var distanceY = window.innerHeight + document.body.scrollTop + 20,
+      let root = document.getElementById('root')
+      let distanceY = window.innerHeight + document.body.scrollTop + 20,
           shrinkOn = root.offsetHeight,
           footer = document.getElementById("footer");
 
-      console.log(distanceY, shrinkOn)
       if (distanceY > shrinkOn) {
           footer.classList.add("Footer__expand");
       } else {
