b39cae1d2c64b7fd62a8394355a20a80fe423013
Calum Butler <calum@flashx.io>
2017-10-30 16:43:44 -0400

    added chunkUpload utility for uploading large files

---
+++ src/js/utils/ChunkUploader.js
@@ -0,0 +1,50 @@
+//vendor
+import uuidv4 from 'uuid/v4'
+//mutations
+import ImportLabbookMutation from 'Mutations/ImportLabbookMutation'
+
+const uploadChunk = (chunk) => {
+  console.log(uuidv4, chunk)
+}
+
+
+const ChunkUploader = {
+
+  chunkFile: (file) => {
+    const id = uuidv4()
+    const chunk = 1024 * 1024 * 4;
+    const size = file.size;
+    let fileLoadedSize = 0;
+    let chunkNumber = 1;
+    let totalChunks = Math.ceil(file.size/chunk);
+
+
+    function getChunk(file){
+      let sliceUpperBound = (size > (fileLoadedSize + chunk)) ? (fileLoadedSize + chunk) : ((size - fileLoadedSize) + fileLoadedSize)
+
+      let blob = file.slice(fileLoadedSize, sliceUpperBound)
+
+      uploadChunk({
+        blob:blob,
+        size: size,
+        chunck_size: (sliceUpperBound - fileLoadedSize),
+        total_chunks: totalChunks,
+        chunk_number: chunkNumber,
+        file_name: file.name,
+        id: id
+      })
+
+      fileLoadedSize = fileLoadedSize + chunk;
+
+      if(size > sliceUpperBound){
+        chunkNumber++
+        getChunk(file, fileLoadedSize )
+      }
+    }
+
+    getChunk(file)
+
+  }
+}
+
+export default ChunkUploader
