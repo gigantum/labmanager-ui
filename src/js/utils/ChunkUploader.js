f6f1f5e6f3aa55f862ea18e2068d91379f245024
Calum Butler <calum@flashx.io>
2017-11-06 20:13:41 -0500

    added settimeout to send mutation if a response is not found within 60 seconds

--- src/js/utils/ChunkUploader.js
+++ src/js/utils/ChunkUploader.js
@@ -4,10 +4,48 @@ import uuidv4 from 'uuid/v4'
 import JobStatus from './JobStatus'
 //mutations
 import ImportLabbookMutation from 'Mutations/ImportLabbookMutation'
+import AddLabbookFileMutation from 'Mutations/AddLabbookFileMutation'
+let requestFlag = false;
+let uploadAttempts = 0;
+const uploadLabbookChunk = (file, chunk, accessToken, username, filepath, getChunkCallback, componentCallback) => {
 
-const uploadChunk = (file, chunk, accessToken, username, filepath, getChunkCallback, componentCallback) => {
 
   ImportLabbookMutation(username, username, chunk.blob, chunk, accessToken, (result, error)=>{
+      requestFlag = true;
+      uploadAttempts = 0;
+      // console.log(result, error)
+      if(result && (error === undefined)){
+        getChunkCallback(file, result)
+      }else{
+        getChunkCallback(error)
+      }
+
+  })
+
+}
+
+const uploadFileBrowserChunk = (data, file, chunk, accessToken, username, filepath, getChunkCallback, componentCallback) => {
+  console.log(
+    data.connectionKey,
+    username,
+    username,
+    data.labbookName,
+    data.labbookId,
+    filepath,
+    chunk,
+    accessToken
+  )
+  AddLabbookFileMutation(
+    data.connectionKey,
+    username,
+    username,
+    data.labbookName,
+    data.labbookId,
+    filepath,
+    chunk,
+    accessToken,
+    (result, error)=>{
+      console.log(result, error)
       if(result && (error === undefined)){
         getChunkCallback(file, result)
       }else{
@@ -32,9 +70,9 @@ const ChunkUploader = {
       }
 
     const id = uuidv4(),
-          chunkSize = 1024 * 1024 * 48,
+          chunkSize = 1000 * 1000 * 48,
           fileSize = file.size,
-          fileSizeKb = Math.round(fileSize/1024);
+          fileSizeKb = Math.round(fileSize/1000);
 
     let fileLoadedSize = 0,
         chunkIndex = 0,
@@ -46,7 +84,7 @@ const ChunkUploader = {
 
     const getChunk = (response, result) => {
 
-      if(response.name){ //checks if response is a file
+      if(response.name && (uploadAttempts < 4)){ //checks if response is a file
 
         let sliceUpperBound = (fileSize > (fileLoadedSize + chunkSize))
             ? (fileLoadedSize + chunkSize)
@@ -59,7 +97,7 @@ const ChunkUploader = {
 
         let chunkData =   {
             blob: blob,
-            fileSizeKb: Math.round(fileSizeKb/1024) ,
+            fileSizeKb: Math.round(fileSizeKb/1000, 10) ,
             chunkSize: chunkSize,
             totalChunks: totalChunks,
             chunkIndex: chunkIndex - 1,
@@ -68,17 +106,62 @@ const ChunkUploader = {
           }
 
         if(chunkIndex <= totalChunks){ //if  there is still chunks to process do next chunk
-          uploadChunk(
-            file,
-            chunkData,
-            data.accessToken,
-            username,
-            filepath,
-            getChunk,
-            componentCallback
-          )
+          //select type of mutation
+          if(file.name.indexOf('.lbk') > -1){
 
-          postMessage(chunkData) //post progress back to worker instantiator file
+            uploadLabbookChunk(
+              file,
+              chunkData,
+              data.accessToken,
+              username,
+              filepath,
+              getChunk,
+              componentCallback
+            )
+            let uploadLabbookChunkCounter = 0;
+
+            let uploadInterval = setInterval(()=>{
+
+              uploadLabbookChunkCounter += 5;
+
+              if(uploadLabbookChunkCounter > 60){
+                uploadAttempts++;
+                uploadLabbookChunk(
+                  file,
+                  chunkData,
+                  data.accessToken,
+                  username,
+                  filepath,
+                  getChunk,
+                  componentCallback
+                )
+                clearInterval(uploadInterval);
+              }
+
+              if(requestFlag){
+                clearInterval(uploadInterval);
+                requestFlag = false
+              }
+            }, 5000);
+
+
+            postMessage(chunkData) //post progress back to worker instantiator file
+
+          }
+          else{
+            uploadFileBrowserChunk(
+              data,
+              file,
+              chunkData,
+              data.accessToken,
+              username,
+              filepath,
+              getChunk,
+              componentCallback
+            )
+
+            postMessage(chunkData)
+          }
 
         }else if(result){ //completes chunk upload task
 
@@ -87,7 +170,12 @@ const ChunkUploader = {
           componentCallback(response)
         }
       }else{ //chunk upload fails
-        componentCallback(response)
+        if(uploadAttempts > 2){
+          console.log('Too Many Failed Attempts')
+          componentCallback([{message: 'Too many failed attempts'}])
+        }else{
+          componentCallback(response)
+        }
       }
     }
 
@@ -100,6 +188,8 @@ const ChunkUploader = {
   waits for data to be passed before starting chunking
 */
 onmessage = (evt) => {
+  console.log(evt)
+  evt.preventDefault();
   ChunkUploader.chunkFile(evt.data)
 }
 export default ChunkUploader
