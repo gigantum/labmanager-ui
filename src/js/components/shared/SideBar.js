bbfed54bd82230f76d2362f7aa1722b6bab7d214
Maz <maz@Mazs-MacBook-Pro.local>
2018-03-12 16:39:20 -0400

    changed isauthenticated back

--- src/js/components/shared/SideBar.js
+++ src/js/components/shared/SideBar.js
@@ -48,7 +48,7 @@ export default class SideBar extends Component {
           </ul>
 
           {
-            authed && (
+            isAuthenticated() && (
                 <User {...this.props} />
               )
           }
