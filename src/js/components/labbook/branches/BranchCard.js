24de2fd0230612c06bbf446ff97aa418a2a44f2d
Calum Butler <calum@flashx.io>
2018-04-12 15:51:31 -0400

    stash changes for pull

--- src/js/components/labbook/branches/BranchCard.js
+++ src/js/components/labbook/branches/BranchCard.js
@@ -131,7 +131,7 @@ export default class BranchCard extends Component {
           })
 
         }
-        if(response.success){
+        if(response){
           store.dispatch({
             type: 'INFO_MESSAGE',
             payload:{
