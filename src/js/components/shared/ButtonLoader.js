9101a4f7bf8d544cfb16158a7f4f14269913aa36
Calum Butler <calum@flashx.io>
2018-05-16 14:42:14 -0400

    fixed scroll to close branches menu and fixed button loader not disabling

--- src/js/components/shared/ButtonLoader.js
+++ src/js/components/shared/ButtonLoader.js
@@ -4,6 +4,23 @@ import classNames from 'classnames'
 
 export default class ButtonLoader extends Component {
 
+  constructor(props){
+  	super(props);
+  	this.state = {};
+
+    this._clickCallback = this._clickCallback.bind(this)
+  }
+
+  /****
+   * @param {object, object}
+   * checks button state
+   * passes back to callback
+   * return {}
+   ******/
+  _clickCallback(evt, params){
+    this.props.clicked(evt, params)
+  }
+
   render() {
 
     const {
@@ -23,9 +40,9 @@ export default class ButtonLoader extends Component {
 
     return (
       <button
-        disabled={buttonDisabled}
+        disabled={buttonState !== ''}
         className={buttonLoaderCSS}
-        onClick={(evt) => this.props.clicked(evt, params)}>
+        onClick={(evt => this.props.clicked(evt, params))}>
 
         {buttonTestToDisplay}
 
