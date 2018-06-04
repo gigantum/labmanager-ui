6de39a2555f9f061c13648bc408e1750f3034885
Calum Butler <calum@flashx.io>
2018-06-04 12:59:56 -0400

    addded profile

--- src/js/components/Routes.js
+++ src/js/components/Routes.js
@@ -14,6 +14,7 @@ import Footer from 'Components/shared/Footer';
 import Prompt from 'Components/shared/Prompt';
 import Labbook from 'Components/labbook/Labbook';
 import Loader from 'Components/shared/Loader'
+import Profile from 'Components/profile/Profile'
 //
 import store from 'JS/redux/store'
 
@@ -215,6 +216,17 @@ export default class Routes extends Component {
                     }}
                   />
 
+                  <Route
+                    path="/profile"
+                    render={(props)=>{
+                      return(
+                        <Profile
+
+                        />
+                      )
+                    }}
+                  />
+
                   <Prompt
                     ref="prompt"
                   />
@@ -232,7 +244,7 @@ export default class Routes extends Component {
     } else {
       return (
         <div className="Routes__error">
-     
+
           <p>An error has occured. Please try refreshing the page.</p>
         </div>
       )
---
+++ src/js/components/profile/Profile.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+  //login for Auth0 function
+  constructor(props){
+    super(props)
+  }
+
+  render() {
+    return(
+      <div>profile
+      </div>
+    )
+  }
+}
