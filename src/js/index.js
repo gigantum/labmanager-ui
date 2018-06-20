3afea736721160b880db32bff3bc6a65666b3795
Maz <maz@Mazs-MacBook-Pro.local>
2018-06-20 11:01:34 -0400

    potetional fix

--- src/js/index.js
+++ src/js/index.js
@@ -3,12 +3,14 @@ import { render } from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import './../css/critical.scss';
 import UserIdentity from 'JS/Auth/UserIdentity'
+import Auth from 'JS/Auth/Auth';
 
 // components
 import Routes from './components/Routes';
 
-UserIdentity.getUserIdentity().then((response)=>{
+UserIdentity.getUserIdentity().then((response, error)=>{
   let expiresAt = JSON.stringify((new Date().getTime() * 1000) + new Date().getTime());
+  console.log(response, error)
   if(response.data){
     if(response.data.userIdentity){
       localStorage.setItem('family_name', response.data.userIdentity.familyName);
@@ -23,9 +25,11 @@ UserIdentity.getUserIdentity().then((response)=>{
       localStorage.removeItem('username')
       localStorage.removeItem('expires_at')
     }
-  }else if(response.error){
+  }else if(response.errors){
+    Auth.login();
     console.error(response.errors[0].message)
   }else{
+    Auth.login();
     console.error(response)
   }
 })
