3aac3e2e57f6822d20cfffb4380907e8aa82139e
calumbutler <calum@gigantum.com>
2018-07-12 15:31:42 -0400

    update

--- src/js/createRelayEnvironment.js
+++ src/js/createRelayEnvironment.js
@@ -36,7 +36,7 @@ function fetchQuery(
   let headers = {
       'accept': '*/*',
       'Access-Control-Allow-Origin': '*'
-  }
+  } 
 
   if(uploadables && uploadables[0]){
     if(uploadables[1]){
@@ -59,7 +59,7 @@ function fetchQuery(
       localStorage.setItem('id_token', hashObj.id_token)
 
       window.location.hash = ''
- 
+
     } else if(localStorage.getItem('access_token')){
 
        const accessToken = localStorage.getItem('access_token')
